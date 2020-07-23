ac60c792748eb47385dc4c4acd73a3c6ffdb4ca0
Diyana Elyani Azman <diyana@nakhodatech.my>
2020-07-23 22:40:23 +0800

    create todolist app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
-  title = 'my-todo-list';
-}
+import { Component } from '@angular/core'; 
+  
+@Component({ 
+    selector: 'app-root', 
+    templateUrl: './app.component.html', 
+    styleUrls: ['./app.component.scss'] 
+}) 
+export class AppComponent { 
+  
+    /* An empty array that is responsible 
+       to add a division */
+    public items = []; 
+  
+    /* A two-way binding performed which 
+       pushes text on division */
+    public newTask; 
+  
+  
+    /* When input is empty, it will 
+       not create a new division */
+    public addToList() { 
+        if (this.newTask == '') { 
+        } 
+        else { 
+            this.items.push(this.newTask); 
+            this.newTask = ''; 
+        } 
+    } 
+  
+    /* This function takes to input the 
+       task, that has to be deleted*/
+    public deleteTask(index, task) { 
+        console.log("test", task);
+        if(confirm(`Are you sure to delete ${task}?`)) {
+          this.items.splice(index, 1); 
+        }
+    } 
+} 
